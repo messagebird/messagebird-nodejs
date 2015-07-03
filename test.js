24765d00150e3ef083cf2968e7b0134c95cb1f73
Joey van Ommen <joey@messagebird.com>
2015-07-04 01:44:42 +0200

    Enable all tests and create better test run TODO: Document each test properly

--- test.js
+++ test.js
@@ -1,53 +1,52 @@
 var messagebird = require('./lib/messagebird');
 
+console.log('MessageBird Node.js SDK tests');
+console.log('---');
+
 // test_WyB2x1bFsXypeab533ToUj0ql
 // live_kqnckmyT61aVkpgObVUN4cNcD
 messagebird.init('test_WyB2x1bFsXypeab533ToUj0ql');
 
-/*
+console.log('Sending message(s)...');
+
 // Send an SMS
 messagebird.messages.create('Joey', [ 31610948431 ], 'This is a test message', {}, function(error, response){
-    console.log(response);
-});
-*/
+    console.log('Messages sent - ' + response.id);
 
-/*
-// Read an SMS
-messagebird.messages.read('4ba78930655970b77b9c673b97155895', function(error, response){
-    console.log(response);
+    // Read an SMS
+    messagebird.messages.read(response.id, function(error, response){
+        console.log('Messages sent/read: ' + response.totalCount);
+    });
 });
-*/
 
+console.log('Sending voice message(s)...');
 
 // Send a voice message
 messagebird.voice_messages.create([ 31610948431 ], 'This is a test message', {}, function(error, response){
-    console.log(response);
+    console.log('Voice messages sent - ' + response.id);
+
+    // Read an SMS
+    messagebird.voice_messages.read(response.id, function(error, response){
+        console.log('Voice messages sent/read: ' + response.totalCount);
+    });
 });
 
+console.log('Creating HLR...');
 
-/*
-// Read a voice message
-messagebird.voice_messages.read('d85384a0355970d6f495032a58211748', function(error, response){
-    console.log(response);
-}); // No is working yet
-*/
-
-/*
 // Create HLR
 messagebird.hlr.create(31610948431, 'The ref', {}, function(error, response){
-    console.log(response);
-});
-*/
+    console.log('HLR sent - ' + response.id);
 
-/*
-// Read HLR
-messagebird.hlr.read('dce24d60655970ed6ef9620h64451942', function(error, response){
-    console.log(response);
+    // Read an SMS
+    messagebird.hlr.read(response.id, function(error, response){
+        console.log('HLR sent/read: ' + response.totalCount);
+    });
 });
-*/
 
-/*
+console.log('Reading balance...');
+
 messagebird.balance.read(function(error, response){
-    console.log(response);
+    console.log('Your balance: ' + response.amount + ', ' + response.type + ', ' + response.payment );
 });
-*/
+
+console.log('---');
