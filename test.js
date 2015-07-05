5835656490305bae7feea07c0ffcafa28731cb9d
Joey van Ommen <joey@messagebird.com>
2015-07-05 11:45:10 +0200

    Better error handling, enabled strict mode and added debug_mode

--- test.js
+++ test.js
@@ -3,9 +3,14 @@ var messagebird = require('./lib/messagebird');
 console.log('MessageBird Node.js Library test run');
 console.log('---');
 
-// test_WyB2x1bFsXypeab533ToUj0ql
-// live_kqnckmyT61aVkpgObVUN4cNcD
-messagebird.init('test_WyB2x1bFsXypeab533ToUj0ql');
+var live = false;
+if( live ){
+    messagebird.init('live_kqnckmyT61aVkpgObVUN4cNcD');
+} else{
+    messagebird.init('test_WyB2x1bFsXypeab533ToUj0ql', true);
+}
+
+var now = microtime(true);
 
 console.log('Sending message(s)...');
 
@@ -26,15 +31,12 @@ messagebird.messages.create(
     },
     function(error, response){
         if( !error ){
-            console.log('Messages sent - ' + response.id);
+            console.log((microtime(true) - now).toFixed(3) + 's: Messages sent - ' + response.id);
 
             // Read an SMS
             messagebird.messages.read(response.id, function(error, response){
-                console.log('Messages sent/read: ' + response.totalCount);
+                console.log((microtime(true) - now).toFixed(3) + 's: Messages sent/read: ' + response.totalCount);
             });
-        } else {
-            console.log('Oh no, error(s) occured:');
-            console.dir(error);
         }
     });
 
@@ -43,48 +45,53 @@ console.log('Sending voice message(s)...');
 // Send a voice message
 messagebird.voice_messages.create(
     [ 31610948431 ],
-    'Hey Cecil, please vote for Obama Drama in the upcomming elections.',
-    {},
+    'Hey Henri Winkel! You can also send automated messages with Messagebird. Cecil is sooo dumb.',
+    {
+        reference: 'A client reference',
+        language: 'en-gb',
+        voice: 'female',
+        repeat: 1,
+        ifMachine: 'continue',
+        scheduledDatetime: '2015-07-03T19:40:03+00:00'
+    },
     function(error, response){
         if( !error ){
-            console.log('Voice messages sent - ' + response.id);
+            console.log((microtime(true) - now).toFixed(3) + 's: Voice messages sent - ' + response.id);
 
             // Read an SMS
             messagebird.voice_messages.read(response.id, function(error, response){
-                console.log('Voice messages sent/read: ' + response.totalCount);
+                console.log((microtime(true) - now).toFixed(3) + 's: Voice messages sent/read: ' + response.totalCount);
             });
-        } else {
-            console.log('Oh no, error(s) occured:');
-            console.dir(error);
         }
     });
 
 console.log('Creating HLR...');
 
 // Create HLR
-messagebird.hlr.create(31610948431, 'The ref', {}, function(error, response){
-    if( !error ){
-        console.log('HLR sent - ' + response.id);
+messagebird.hlr.create(
+    31610948431,
+    'The ref',
+    function(error, response){
+        if( !error ){
+            console.log((microtime(true) - now).toFixed(3) + 's: HLR sent - ' + response.id);
 
-        // Read an SMS
-        messagebird.hlr.read(response.id, function(error, response){
-            console.log('HLR sent/read: ' + response.totalCount);
-        });
-    } else {
-        console.log('Oh no, error(s) occured:');
-        console.dir(error);
-    }
-});
+            // Read an SMS
+            messagebird.hlr.read(response.id, function(error, response){
+                console.log((microtime(true) - now).toFixed(3) + 's: HLR sent/read: ' + response.totalCount);
+            });
+        }
+    });
 
 console.log('Reading balance...');
 
 messagebird.balance.read(function(error, response){
     if( !error ){
-        console.log('Your balance: ' + response.amount + ', ' + response.type + ', ' + response.payment );
-    } else {
-        console.log('Oh no, error(s) occured:');
-        console.dir(error);
+        console.log((microtime(true) - now).toFixed(3) + 's: Your balance: ' + response.amount + ', ' + response.type + ', ' + response.payment );
     }
 });
 
 console.log('---');
+
+function microtime() {
+    return parseFloat(new Date().getTime() / 1000);
+}
