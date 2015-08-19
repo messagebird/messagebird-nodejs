651a64e00b9e9a1c53a0245a3505e2b7501088b0
Franklin van de Meent <fr@nkl.in>
2015-08-19 20:27:39 +0200

    Fix lint errors in balance example

--- examples/balance.js
+++ examples/balance.js
@@ -1,6 +1,6 @@
 
 var messagebird = require('messagebird');
 
-messagebird.balance.read(function( error, response ){
-    console.log('Your balance: ' + response.amount + ', ' + response.type + ', ' + response.payment );
+messagebird.balance.read (function (error, response) {
+  console.log ('Your balance: ' + response.amount + ', ' + response.type + ', ' + response.payment);
 });
