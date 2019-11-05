935f0423ef8022014f0ebc4a862d4f83cc12826f
William Wagner Moraes Artero <william.moraesartero@messagebird.com>
2019-11-05 10:15:53 +0100

    Add voice transcriptions

---
+++ examples/transcription.js
@@ -0,0 +1,34 @@
+
+var messagebird = require('messagebird')('<YOUR_ACCESS_KEY>');
+
+messagebird.transcriptions.create('<CALL_ID>', '<LEG_ID>', '<RECORDING_ID>', '<LANGUAGE>', function (err, data) {
+  if (err) {
+    return console.log(err);
+  }
+
+  console.log(data);
+});
+
+messagebird.transcriptions.list('<CALL_ID>', '<LEG_ID>', '<RECORDING_ID>', function (err, data) {
+  if (err) {
+    return console.log(err);
+  }
+
+  console.log(data);
+});
+
+messagebird.transcriptions.read('<CALL_ID>', '<LEG_ID>', '<RECORDING_ID>', '<TRANSCRIPTION_ID>', function (err, data) {
+  if (err) {
+    return console.log(err);
+  }
+
+  console.log(data);
+});
+
+messagebird.transcriptions.download('<CALL_ID>', '<LEG_ID>', '<RECORDING_ID>', '<TRANSCRIPTION_ID>', function (err, data) {
+  if (err) {
+    return console.log(err);
+  }
+
+  console.log(data);
+});
