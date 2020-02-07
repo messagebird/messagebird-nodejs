853f1a35efda51fe91181fee8436dca7217a42f3
Serge Kamel <sergekamel@Serges-MacBook-Pro.local>
2020-02-07 15:17:21 +0100

    test: add voice messages listing example.

--- examples/voice_messages_read.js
+++
@@ -1,9 +0,0 @@
-
-var messagebird = require('messagebird')('<YOUR_ACCESS_KEY>');
-
-messagebird.voice_messages.read('<VOICE_MESSAGE_ID>', function (err, data) {
-  if (err) {
-    return console.log(err);
-  }
-  console.log(data);
-});
